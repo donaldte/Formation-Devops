0577cdca27f434b76db6db917dfc28f81ca13ff7
donaldte <donaldtedom0@gmail.com>
2025-02-25 17:36:28 +0100

    fix: base du shell

---
+++ shell/base.sh
@@ -0,0 +1,13 @@
+# Linux and shell scripting 
+# OS: is between your software and hardware 
+# Linux is the best because it’s Free, Open source, secure, many distributions, fast 
+
+# Fundamental of shell scripting 
+# Shell is the way you talk to your OS using command line (it’s what we call shell commands)
+
+# Manual  automate 
+
+# To write shell scripting you need a file like first-shell-scritp.sh 
+# Ls , ls -ltr, man , touch , mkdir , top , df -h , nproc, 
+#!/bin/sh/ name shebang (bash, dash, sh, ksh)
+
