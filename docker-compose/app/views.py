74f0e090955c678ac6bad6d8939b55092828ce88
donaldte <donaldtedom0@gmail.com>
2025-04-27 15:56:29 +0100

    fix: refactor

--- docker-compose/app/views.py
+++ docker-compose/app/views.py
@@ -1,7 +1,27 @@
 # app/views.py
 from django.http import HttpResponse
+import socket
 import os
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def get_container_ip():
+    hostname = socket.gethostname()
+    return socket.gethostbyname(hostname)
+
+
 
 def server_identity(request):
     webserver = os.environ.get('SERVER_NAME', 'web1')
-    return HttpResponse(f"Hello from server {webserver}")
+    dictc={
+        'client_ip': get_client_ip(request),
+        'container_ip': get_container_ip()
+    }
+    return HttpResponse(f"Client IP: {dictc['client_ip']}<br>Container IP: {dictc['container_ip']}<br>Webserver: {webserver}")
