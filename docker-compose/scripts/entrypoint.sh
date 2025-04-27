21ec8029358edd4b3bc8974eeb3d3cae733b45af
donaldte <donaldtedom0@gmail.com>
2025-04-27 11:24:24 +0100

    feat: refactor

--- docker-compose/scripts/entrypoint.sh
+++ docker-compose/scripts/entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate
 
 echo "Starting server on 0.0.0.0:8000..."
 
-exec gunicorn app.wsgi:application --bind 0.0.0.0:8000
+exec gunicorn application_web.wsgi:application --bind 0.0.0.0:8000
