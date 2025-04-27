2b59489a82fe2fb91c421479a52a97b97b88690a
donaldte <donaldtedom0@gmail.com>
2025-04-27 12:01:53 +0100

    fix: refactor

--- docker-compose/scripts/entrypoint.sh
+++ docker-compose/scripts/entrypoint.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 # make migration
-echo "Running migrations..."
-python manage.py migrate
+#!/bin/bash
+
+# Si le conteneur est "web1", il fait les migrations
+if [ "$SERVER_NAME" = "web1" ]; then
+    echo "Running migrations from web1..."
+    python manage.py migrate --noinput
+fi
 
 echo "Starting server on 0.0.0.0:8000..."
 
