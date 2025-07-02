9f6f1c20e866e54e4bd446ea87d3a73b346d80cc
donaldte <donaldtedom0@gmail.com>
2025-07-02 11:49:42 +0100

    feat: configmap via volume

--- kubernetes/configmap/exemple/kube-flask-configmap/app.py
+++ kubernetes/configmap/exemple/kube-flask-configmap/app.py
@@ -1,12 +1,13 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
-PORT = int(os.environ.get("FLASK_PORT", 5000))
+
+with open("/config/FLASK_PORT") as f:
+    PORT = int(f.read().strip())
 
 @app.route("/")
 def hello():
-    return f"Hello from Flask running on port {PORT}"
+    return f"Hello from Flask (read from file) on port {PORT}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=PORT)
